8156e98c4c906793e78560dd0b63d6dd0de72a99
Zaid Chamorro <zaid.chamorro@udla.edu.ec>
2026-01-06 18:12:41 -0500

    funciones.h

--- funciones.h
+++ funciones.h
@@ -1,55 +1,41 @@
+#include <stdio.h>
 
+//ESTRUCTURAS
 
-#define MAX_VEHICULOS 100
-#define MAX_CLIENTES 100
-#define MAX_VENTAS 100
-#define MAX_STRING 50
-
-// Estructuras de datos
 typedef struct {
     int id;
-    char marca[MAX_STRING];
-    char modelo[MAX_STRING];
-    int anio;
-    char tipo[MAX_STRING]; // Sedan, SUV, Camioneta, etc.
-    char condicion[MAX_STRING]; // Nuevo, Usado
+    char tipo[20];
+    char marca[20];
+    char condicion[10];
     float precio;
-    int disponible; // 1 = disponible, 0 = vendido
+    int disponible;
 } Vehiculo;
 
 typedef struct {
     int id;
-    char nombre[MAX_STRING];
+    char nombre[50];
     int edad;
-    char telefono[MAX_STRING];
+    float presupuesto;
 } Cliente;
 
 typedef struct {
-    int id;
-    int id_cliente;
-    int id_vehiculo;
-    char fecha[MAX_STRING];
-    float precio_venta;
+    int idVenta;
+    int idVehiculo;
+    int idCliente;
 } Venta;
 
-// Variables globales externas
-extern Vehiculo inventario[MAX_VEHICULOS];
-extern Cliente clientes[MAX_CLIENTES];
-extern Venta ventas[MAX_VENTAS];
-extern int num_vehiculos;
-extern int num_clientes;
-extern int num_ventas;
 
-// Prototipos de funciones
-void menu_principal();
-void cargar_datos();
-void guardar_datos();
-void agregar_vehiculo();
-void listar_vehiculos();
-void buscar_vehiculos_filtros();
-void registrar_cliente();
-void registrar_venta();
-void listar_clientes();
-void listar_ventas();
-void limpiar_buffer();
-void to_lowercase(char *str);
+
+void registrarVehiculo();
+void listarVehiculos();
+void buscarVehiculo();
+void registrarCliente();
+void listarClientes();     
+void registrarVenta();
+
+
+
+int existeVehiculo(int id);
+int existeCliente(int id);
+int vehiculoDisponible(int id);
+
